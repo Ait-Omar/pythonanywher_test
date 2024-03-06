6cfb704a984d0526c3f9f6183904e81fcf7f4b21
AIT-OMAR mohamed <aitomar.mip.97@gmail.com>
2024-03-06 14:43:36 +0100

    modified file

--- app.py
+++ app.py
@@ -5,5 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+@app.route("/base")
+def base():
+    return "<p>this is the home page</p>"
 if __name__=="__main__":
    app.run(debug=True)
